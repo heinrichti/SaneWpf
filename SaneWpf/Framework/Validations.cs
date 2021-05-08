f3761fbf465ed2d22098423d2e74abea3570470d
Tia Heinrich <2929650+heinrichti@users.noreply.github.com>
2021-05-08 22:44:44 +0200

    Call is only for generating, remove from release build

--- SaneWpf/Framework/Validations.cs
+++ SaneWpf/Framework/Validations.cs
@@ -1,10 +1,12 @@
 ﻿using System;
+using System.Diagnostics;
 using System.Linq.Expressions;
 
 namespace SaneWpf.Framework
 {
     public static class Validations
     {
+        [Conditional("DEBUG")]
         public static void Add<T, TU>(
             T viewModel,
             Expression<Func<T, TU>> property,
